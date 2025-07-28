0ddf0e0b0e81d89cdf706f91db3f2c0dad5f5f44
Jack Spencer Walton <132408856+jack-walton@users.noreply.github.com>
2025-07-27 21:07:20 -0500

    Update website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Jack Walton',
+    template: '%s | Jack Walton',
   },
   description: 'This is my portfolio.',
   openGraph: {
